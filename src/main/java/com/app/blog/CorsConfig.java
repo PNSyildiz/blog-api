fe8dd4f46770118d4dfa50e3b5f68b48ff4f19aa
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-01-09 00:19:47 +0200

    changed origin

--- src/main/java/com/app/blog/CorsConfig.java
+++ src/main/java/com/app/blog/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("//blog-production-8bc9.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "UPDATE", "PUT"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 
